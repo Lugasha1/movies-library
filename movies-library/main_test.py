06bb2af262452aaa87ca2a8212d05a03be20616b
Mike Lugovoi <lugovoj.s.i.74@gmail.com>
2025-01-29 15:35:48 +0100

    Initial commit: добавлен основной код

---
+++ movies-library/main_test.py
@@ -0,0 +1,24 @@
+import unittest
+from main import MoviesLibrary
+
+
+class TestMoviesLibrary(unittest.TestCase):
+    def test_add_movie(self):
+        library = MoviesLibrary(['Комедия'])
+        library.add_movie('Комедия', 'Фильм 1')
+        self.assertEqual(library.recommend('Комедия'), ['Фильм 1'])
+
+    def test_recommend_empty_genre(self):
+        library = MoviesLibrary(['Комедия'])
+        self.assertEqual(library.recommend('Комедия'), [])
+
+    def test_shared_movie_list_bug(self):
+        library = MoviesLibrary(['Комедия', 'Ужасы'])
+        library.add_movie('Комедия', 'Фильм 1')
+        library.add_movie('Ужасы', 'Фильм 2')
+        self.assertEqual(library.recommend('Комедия'), ['Фильм 1'])
+        self.assertEqual(library.recommend('Ужасы'), ['Фильм 2'])  # Ожидаемая ошибка
+
+
+if __name__ == '__main__':
+    unittest.main()
